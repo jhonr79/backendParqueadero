c5579a46a20a13c869f819a311536c9f5a060136
jhonr467 <jhonr467@gmail.com>
2018-08-27 16:46:33 -0500

    configuracion build

---
+++ build/resources/main/import.sql
@@ -0,0 +1,9 @@
+--caso prueba 1: Si el carro permaneció un día y 3 horas se debe cobrar 11.000
+--ejecutada el dia 2018-08-22 14:45:00
+--insert into parqueaderos (placa,tipo,cilindraje,fecha_ingreso,fecha_salida) values ('DKX16E',1,150, {ts '2018-08-21 07:00:00.01'}, null);
+
+--caso prueba 2: Si la moto permaneció un 10 horas y es de 650CC se cobra 6.000
+--ejecutada el dia 2018-08-22 14:59:00
+--insert into parqueaderos (placa,tipo,cilindraje,fecha_ingreso,fecha_salida) values ('DKX16E',2,650, {ts '2018-08-22 00:00:00.01'}, null);
+
+insert into parqueaderos (placa,tipo,cilindraje,fecha_ingreso,fecha_salida) values ('DKX16E',2,650, {ts '2018-08-22 00:00:00.01'}, null);
